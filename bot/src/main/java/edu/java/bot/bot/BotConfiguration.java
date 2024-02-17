2da2b3df505627dd1aa327c1ccccbe55410e1dbb
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-02-17 22:53:02 +0300

    Hw1: create bot configuration

---
+++ bot/src/main/java/edu/java/bot/bot/BotConfiguration.java
@@ -0,0 +1,22 @@
+package edu.java.bot.bot;
+
+import com.pengrad.telegrambot.TelegramBot;
+import edu.java.bot.configuration.ApplicationConfig;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Log4j2
+@RequiredArgsConstructor
+@Configuration
+public class BotConfiguration {
+
+    private final ApplicationConfig applicationConfig;
+
+    @Bean
+    public TelegramBot telegramBot() {
+        return new TelegramBot(applicationConfig.telegramToken());
+    }
+
+}
