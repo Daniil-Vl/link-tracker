87932e61e601729029701268af1c98e82053531f
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-02-21 20:07:44 +0300

    Hw1: replace list of available commands with list from CommandManager

--- bot/src/main/java/edu/java/bot/commands/HelpCommand.java
+++ bot/src/main/java/edu/java/bot/commands/HelpCommand.java
@@ -2,22 +2,21 @@ package edu.java.bot.commands;
 
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import edu.java.bot.commands.CommandManager.CommandManager;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Log4j2
-@RequiredArgsConstructor
 @Component
 public class HelpCommand implements Command {
 
-    private List<Command> availableCommands = List.of(
-        new StartCommand(null),
-        new ListCommand(null),
-        new TrackCommand(null, null),
-        new UntrackCommand(null, null)
-    );
+    private final CommandManager commandManager;
+
+    public HelpCommand(@Lazy CommandManager commandManager) {
+        this.commandManager = commandManager;
+    }
 
     @Override
     public String command() {
@@ -35,11 +34,12 @@ public class HelpCommand implements Command {
         stringBuilder.append("These bot is designed to help you track links from various internet resources\n\n");
         stringBuilder.append("You can control me by sending these commands: \n");
 
+        List<Command> availableCommands = commandManager.getAvailableCommands();
+
         for (int ind = 0; ind < availableCommands.size(); ind++) {
             Command command = availableCommands.get(ind);
             stringBuilder.append("%d) %s - %s\n".formatted(ind + 1, command.command(), command.description()));
         }
-        stringBuilder.append("%d) %s - %s".formatted(availableCommands.size() + 1, command(), description()));
 
         return stringBuilder.toString();
     }
