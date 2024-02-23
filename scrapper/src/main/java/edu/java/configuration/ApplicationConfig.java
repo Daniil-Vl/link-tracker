74fb24841d9904c97eeba6ca09fd798919150281
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-02-24 00:23:56 +0300

    Hw2: add LinkUpdaterScheduler

--- scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/java/configuration/ApplicationConfig.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
+    @Bean
     @NotNull
     Scheduler scheduler,
     @NotNull
