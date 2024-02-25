c75768ebadbf9d109dd8b7ea9595e021eee87f39
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-02-25 19:12:02 +0300

    Hw2: add default headers (bearer auth, accept json) to github webclient

--- scrapper/src/main/java/edu/java/client/github/GithubClientImpl.java
+++ scrapper/src/main/java/edu/java/client/github/GithubClientImpl.java
@@ -2,9 +2,12 @@ package edu.java.client.github;
 
 import edu.java.dto.github.GithubEventResponse;
 import java.util.List;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Log4j2
 public class GithubClientImpl implements GithubClient {
     private final WebClient webClient;
     private final String accessToken;
@@ -13,7 +16,14 @@ public class GithubClientImpl implements GithubClient {
     public GithubClientImpl(String baseURL, String accessToken) {
         this.baseURL = baseURL;
         this.accessToken = accessToken;
-        this.webClient = WebClient.builder().baseUrl(baseURL).build();
+        this.webClient =
+            WebClient.builder()
+                .baseUrl(baseURL)
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.setBearerAuth(accessToken);
+                    httpHeaders.setAccept(List.of(MediaType.APPLICATION_JSON));
+                })
+                .build();
     }
 
     public GithubClientImpl(String accessToken) {
@@ -25,16 +35,12 @@ public class GithubClientImpl implements GithubClient {
     public List<GithubEventResponse> getLatestUpdates(String author, String repository, int numberOfUpdates) {
         String endpoint = "/networks/%s/%s/events?per_page=%s".formatted(author, repository, numberOfUpdates);
 
-        List<GithubEventResponse> responses = webClient
+        return webClient
             .get()
             .uri(endpoint)
-            .header("Authorization", "Bearer " + accessToken)
-            .header("Accept", "application/json")
             .retrieve()
             .bodyToMono(new ParameterizedTypeReference<List<GithubEventResponse>>() {
             })
             .block();
-
-        return responses;
     }
 }
