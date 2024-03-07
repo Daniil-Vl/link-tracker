54cfbb04b330c177f311ca4c1f5092fe9c6c0f28
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-03-07 12:21:52 +0300

    Hw3: remove unnecessary annotation

--- scrapper/src/main/java/edu/java/client/bot/BotClient.java
+++ scrapper/src/main/java/edu/java/client/bot/BotClient.java
@@ -1,9 +1,7 @@
 package edu.java.client.bot;
 
 import edu.java.LinkUpdate;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface BotClient {
     String sendLinkUpdateRequest(LinkUpdate linkUpdate);
 }
