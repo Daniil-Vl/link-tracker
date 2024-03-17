6f82404a0b6ff2e87220f85ac8e07f77394af71d
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-03-17 14:31:13 +0300

    Hw5: add default messages to exceptions

--- scrapper/src/main/java/edu/java/exceptions/ChatNotExistException.java
+++ scrapper/src/main/java/edu/java/exceptions/ChatNotExistException.java
@@ -1,6 +1,12 @@
 package edu.java.exceptions;
 
 public class ChatNotExistException extends Exception {
+    private static final String defaultMessage = "User not authenticated";
+
+    public ChatNotExistException() {
+        super(defaultMessage);
+    }
+
     public ChatNotExistException(String message) {
         super(message);
     }
--- scrapper/src/main/java/edu/java/exceptions/LinkNotExistException.java
+++ scrapper/src/main/java/edu/java/exceptions/LinkNotExistException.java
@@ -1,6 +1,12 @@
 package edu.java.exceptions;
 
 public class LinkNotExistException extends Exception {
+    private static final String defaultMessage = "Link not exist";
+
+    public LinkNotExistException() {
+        super(defaultMessage);
+    }
+
     public LinkNotExistException(String message) {
         super(message);
     }
