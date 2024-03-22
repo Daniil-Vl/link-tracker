57281a745b55464333be300c224647dfd4877335
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-03-22 22:21:50 +0300

    Hw5: add conditionally enabling scheduling

--- scrapper/src/main/java/edu/java/scheduling/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduling/LinkUpdaterScheduler.java
@@ -4,9 +4,7 @@ import edu.java.service.LinkUpdaterService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Service;
 
-@Service
 @Log4j2
 @RequiredArgsConstructor
 public class LinkUpdaterScheduler {
