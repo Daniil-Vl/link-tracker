aba5f8e426418eff162d11093ce0d8870b1b9970
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-03-24 00:14:32 +0300

    Hw5: fix unique url constraint in link table

--- scrapper/src/main/resources/migrations/sql/init_link_table.sql
+++ scrapper/src/main/resources/migrations/sql/init_link_table.sql
@@ -3,11 +3,11 @@
 
 CREATE TABLE link
 (
-    id              BIGINT GENERATED ALWAYS AS IDENTITY NOT NULL,
-    url             VARCHAR(300)                        NOT NULL,
-    updated_at      TIMESTAMP WITH TIME ZONE            NOT NULL,
-    last_check_time TIMESTAMP WITH TIME ZONE            NOT NULL,
+    id              BIGINT GENERATED ALWAYS AS IDENTITY UNIQUE NOT NULL,
+    url             VARCHAR(300) UNIQUE                        NOT NULL,
+    updated_at      TIMESTAMP WITH TIME ZONE                   NOT NULL,
+    last_check_time TIMESTAMP WITH TIME ZONE                   NOT NULL,
 
-    PRIMARY KEY (id),
-    UNIQUE (id, url)
+    PRIMARY KEY (id)
+--     UNIQUE (id, url)
 );
