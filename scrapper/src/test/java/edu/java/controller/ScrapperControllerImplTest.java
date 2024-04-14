f1aadd78d80fbed337445bb61b07af698ca001ad
Daniil-Vl <vlasov.daniil03@gmail.com>
2024-04-14 23:05:06 +0300

    Hw10: add application-test.yml for test with spring context in scrapper

--- scrapper/src/test/java/edu/java/controller/ScrapperControllerImplTest.java
+++ scrapper/src/test/java/edu/java/controller/ScrapperControllerImplTest.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
@@ -29,6 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ScrapperControllerImpl.class)
+@TestPropertySource(properties = "spring.config.location=classpath:application-test.yml")
 class ScrapperControllerImplTest {
     private final ObjectMapper objectMapper = new ObjectMapper();
     @Autowired
--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @SpringBootTest
+@TestPropertySource(properties = "spring.config.location=classpath:application-test.yml")
 public abstract class IntegrationTest {
     protected static PostgreSQLContainer<?> POSTGRES;
 
